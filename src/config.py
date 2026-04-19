a69487b55178885932d9863d8149f36274ca17b0
Juan Murcia <89225595+juanimurcia@users.noreply.github.com>
2026-04-19 20:14:38 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,2 +1,8 @@
 # src/config.py
-DRIVE_FOLDER_ID = '1UaEweoUHsv8H3ILXCFqde-9Rt2kyGufs'
+
+# Configuración del Storage en Supabase
+SUPABASE_BUCKET_NAME = 'Lakehouse'
+
+# Configuración del Dataset de Kaggle
+KAGGLE_DATASET_HANDLE = "shashwatwork/dataco-smart-supply-chain-for-big-data-analysis"
+KAGGLE_FILE_NAME = "DataCoSupplyChainDataset.csv"
